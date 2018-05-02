cb50e5fb23af034a6658fb8955d51168899a0a92
CarlaMorar <carla.morar@yahoo.com>
2018-05-02 21:22:44 +0300

    lab 02.05.2018

---
+++ src/MlApiComp/ClientApp/src/app/services/experiment.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Experiment } from '../models/experiment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ExperimentService {
+  url : string = "http://localhost:52427/api/experiments"
+  constructor(
+    private http: HttpClient
+  ) { }
+  createExperiment(exp:Experiment): Observable<Experiment> {
+
+  return  this.http.post<Experiment>(this.url,exp)  
+  }
+}
--- src/MlApiComp/Controllers/ExperimentsController.cs
+++ src/MlApiComp/Controllers/ExperimentsController.cs
@@ -1,23 +1,24 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MlApiComp.Models;
 
 namespace MlApiComp.Controllers
 {
+  
     [Route("api/[controller]")]
     public class ExperimentsController : Controller
     {
         private static int counter;
-
         [HttpPost]
-        public IActionResult CreateExperiment(Experiment experiment)
+        public IActionResult CreateExperiment([FromBody]Experiment experiment)
         {
             experiment.Id = counter++;
-
             return Ok(experiment);
         }
+
     }
 }
